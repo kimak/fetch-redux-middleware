8ff33256f963f5113debfba65c807f5d5932380f
Kimak <mickael.dumand@gmail.com>
2017-10-05 11:10:57 +0200

    add headers to onSuccess

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 function serialize(obj) {
   const str = [];
-  Object.keys(obj).filter((key) => {
+  Object.keys(obj).filter(key => {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
       str.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
     }
     return key;
   });
-  return str.join('&');
+  return str.join("&");
 }
 
 function goTo(action, store, next, payload, meta) {
-  if (typeof action !== 'function') {
+  if (typeof action !== "function") {
     /* eslint-disable no-console */
-    console.error('fetchMiddleware require a function for onStart, onSuccess, onFailure');
+    console.error(
+      "fetchMiddleware require a function for onStart, onSuccess, onFailure"
+    );
     /* eslint-enable no-console */
     return;
   }
@@ -24,14 +26,14 @@ function goTo(action, store, next, payload, meta) {
 }
 
 let config = {
-  base: '',
+  base: "",
   onRequest: null,
-  defaultParams: null,
+  defaultParams: null
 };
 
-function sendError(action, store, next, result, status) {
+function sendError(action, store, next, result, status, headers) {
   if (action.onError) {
-    goTo(action.onError, store, next, result, { httpCode: status });
+    goTo(action.onError, store, next, result, { httpCode: status, headers });
   }
 
   if (action.autoDispatchPrefix) {
@@ -39,16 +41,16 @@ function sendError(action, store, next, result, status) {
   }
 }
 
-const fetchMiddleware = store => next => (action) => {
+const fetchMiddleware = store => next => action => {
   if (!action.url) {
     return next(action);
   }
   let request = {
     url: action.url,
-    method: action.method || 'GET',
+    method: action.method || "GET",
     headers: action.headers,
     body: action.body,
-    mode: action.mode || 'cors',
+    mode: action.mode || "cors"
   };
 
   if (config.defaultParams || action.params) {
@@ -56,7 +58,10 @@ const fetchMiddleware = store => next => (action) => {
   }
 
   if (config.onRequest) {
-    request = { ...request, ...config.onRequest(request, store.getState(), action) };
+    request = {
+      ...request,
+      ...config.onRequest(request, store.getState(), action)
+    };
   }
 
   if (action.onStart) {
@@ -67,45 +72,74 @@ const fetchMiddleware = store => next => (action) => {
     next({ type: `${action.autoDispatchPrefix}_REQUEST` });
   }
 
-  const baseUrl = typeof action.base === 'string' ? action.base : config.base;
-  const params = request.params ? `?${serialize(request.params)}` : '';
+  const baseUrl = typeof action.base === "string" ? action.base : config.base;
+  const params = request.params ? `?${serialize(request.params)}` : "";
   const { headers, body, method, mode } = request;
   fetch(`${baseUrl}${request.url}${params}`, {
     method,
     headers,
     body: JSON.stringify(body),
-    mode,
+    mode
   })
-  .then((response) => {
-    return response.json().then((result) => {
-      return { status: response.status, ok: response.ok, body: result };
-    }).catch(() => {
-      return { status: response.status, ok: response.ok, body: {} };
-    });
-  })
-  .then((response) => {
-    if (response.ok) {
-      if (action.onSuccess) {
-        goTo(action.onSuccess, store, next, response.body);
+    .then(response => {
+      return response
+        .json()
+        .then(result => {
+          return {
+            status: response.status,
+            ok: response.ok,
+            body: result,
+            headers: response.headers
+          };
+        })
+        .catch(() => {
+          return { status: response.status, ok: response.ok, body: {} };
+        });
+    })
+    .then(
+      response => {
+        if (response.ok) {
+          if (action.onSuccess) {
+            goTo(action.onSuccess, store, next, response.body, {
+              headers: response.headers
+            });
+          }
+          if (action.autoDispatchPrefix) {
+            next({
+              type: `${action.autoDispatchPrefix}_SUCCESS`,
+              payload: response.body
+            });
+          }
+        } else {
+          const result = response.body;
+          if (!response.body.error) {
+            result.error = "UnknownError";
+          }
+          sendError(
+            action,
+            store,
+            next,
+            result,
+            response.status,
+            response.headers
+          );
+        }
+      },
+      response => {
+        sendError(
+          action,
+          store,
+          next,
+          response.body,
+          response.status,
+          response.headers
+        );
       }
-      if (action.autoDispatchPrefix) {
-        next({ type: `${action.autoDispatchPrefix}_SUCCESS`, payload: response.body });
-      }
-    } else {
-      const result = response.body;
-      if (!response.body.error) {
-        result.error = 'UnknownError';
-      }
-      console.log(response);
-      sendError(action, store, next, result, response.status);
-    }
-  }, (response) => {
-    sendError(action, store, next, response.body, response.status);
-  });
+    );
   return null;
 };
 
-export default function (customConfig) {
+export default function(customConfig) {
   config = { ...config, ...customConfig };
   return fetchMiddleware;
 }
